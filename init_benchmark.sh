ad36b6c24cafdf7e4a56719046656703b05eb8cb
gongxini <yigongh@gmail.com>
2022-06-29 00:14:53 -0400

    update memcached result

--- init_benchmark.sh
+++ init_benchmark.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-mkdir -p benchmark
-cd libmemcached
-mkdir -p build
-touch configure.ac aclocal.m4 configure Makefile.am Makefile.in
-cd build
-CXXFLAGS="-pthread" ../configure --enable-memaslap --prefix=$(pwd)/../../benchmark
-make -j4
-if [ $? -ne 0 ]; then
-  echo "Failed to build mysql"
-  exit 1
-fi
-make install
-
+sudo apt-get install -y scons libevent-dev gengetopt 
+git clone git@github.com:leverich/mutilate.git
+cd mutilate
+scons
